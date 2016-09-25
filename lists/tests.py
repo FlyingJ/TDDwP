d15f217aa5ed86564aa0bc4f84287403d32e3ab9
Jason M Harrington <jason.m.harrington@gmail.com>
2016-09-24 21:06:48 -0500

    Add Item create on POST

--- lists/tests.py
+++ lists/tests.py
@@ -53,3 +53,8 @@ class HomePageTest(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_only_saves_item_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
